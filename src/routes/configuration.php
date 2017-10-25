ad043a2239c8aa593d8edf11b21a89f19bd1a5c8
mohini <mohini@mohini>
2017-10-25 18:04:57 +0530

    25-10-2017

--- src/routes/configuration.php
+++ src/routes/configuration.php
@@ -87,7 +87,7 @@ $app->delete('/api/region/delete/{id}', function (Request $request, Response $re
 //Get All Region
 
 $app->get('/api/regions', function (Request $request, Response $response) {
-    $sql = "SELECT * from places";
+    $sql = "SELECT * from places where type='Region'";
     try {
         $db = new db();
         $db = $db->connect();
@@ -386,7 +386,7 @@ $app->delete('/api/city/delete/{id}', function (Request $request, Response $resp
 
 //Get All City
 
-$app->get('/api/city', function (Request $request, Response $response) {
+$app->get('/api/cities', function (Request $request, Response $response) {
     $sql = "SELECT * from places";
     try {
         $db = new db();
@@ -506,3 +506,395 @@ $app->get('/api/areas', function (Request $request, Response $response) {
     }
 });
 ///////////////////////////////////////////////Area Exit///////////////////////////////////////////////
+
+///////////////////////////////////////////////TimeZone//////////////////////////////////////////////////
+
+//Add TimeZone
+
+$app->post('/api/timezone/add', function (Request $request, Response $response) {
+    $title = $request->getParam('title');
+    $value = $request->getParam('value');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO reqs (title,value,code,type,created,modified) VALUES (:title,:value,:code,'Time Zone','$date','$date')";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':value', $value);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "TimeZone Added"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get Single TimeZone Data
+
+$app->get('/api/timezone/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $timezone = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($timezone);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Update TimeZone
+
+$app->put('/api/timezone/update/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $title = $request->getParam('title');
+    $value = $request->getParam('value');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "UPDATE reqs SET title=:title,value=:value,code=:code,modified='$date' WHERE id=$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':value', $value);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Timezone Updated"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Delete TimeZone
+
+$app->delete('/api/timezone/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "DELETE from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "TimeZone Deleted"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get All TimeZone
+
+$app->get('/api/timezones', function (Request $request, Response $response) {
+    $sql = "SELECT * from reqs";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $timezones = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($timezones);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+///////////////////////////////////////////////TimeZone Exit///////////////////////////////////////////////
+
+///////////////////////////////////////////////Currency//////////////////////////////////////////////////
+
+//Add Currency
+
+$app->post('/api/currency/add', function (Request $request, Response $response) {
+    $title = $request->getParam('title');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO reqs (title,code,type,created,modified) VALUES (:title,:code,'Currency','$date','$date')";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Currency Added"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get Single Currency Data
+
+$app->get('/api/currency/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $currency = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($currency);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Update Currency
+
+$app->put('/api/currency/update/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $title = $request->getParam('title');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "UPDATE reqs SET title=:title,code=:code,modified='$date' WHERE id=$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Currency Updated"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Delete Currency
+
+$app->delete('/api/currency/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "DELETE from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Currency Deleted"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get All Currency
+
+$app->get('/api/currencies', function (Request $request, Response $response) {
+    $sql = "SELECT * from reqs";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $currencies = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($currencies);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+///////////////////////////////////////////////Currency Exit///////////////////////////////////////////////
+
+///////////////////////////////////////////////Language//////////////////////////////////////////////////
+
+//Add Language
+
+$app->post('/api/language/add', function (Request $request, Response $response) {
+    $title = $request->getParam('title');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO reqs (title,code,type,created,modified) VALUES (:title,:code,'Language','$date','$date')";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Language Added"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get Single Language Data
+
+$app->get('/api/language/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $language = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($language);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Update Language
+
+$app->put('/api/language/update/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $title = $request->getParam('title');
+    $code = $request->getParam('code');
+    $date = date('Y-m-d H:i:s');
+    $sql = "UPDATE reqs SET title=:title,code=:code,modified='$date' WHERE id=$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':code', $code);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Language Updated"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Delete Language
+
+$app->delete('/api/language/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "DELETE from reqs WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Language Deleted"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get All Language
+
+$app->get('/api/languages', function (Request $request, Response $response) {
+    $sql = "SELECT * from reqs";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $languages = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($languages);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+///////////////////////////////////////////////Language Exit///////////////////////////////////////////////
+
+///////////////////////////////////////////////Benifit//////////////////////////////////////////////////
+
+//Add Benefit
+
+$app->post('/api/benefit/add', function (Request $request, Response $response) {
+    $title = $request->getParam('title');
+    $icon = $request->getParam('icon'); 
+    $type=$request->getParam('type');
+    $date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO benefits (title,icon,type,created,modified) VALUES (:title,:icon,:type,'$date','$date')";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':icon', $icon);
+        $stmt->bindParam(':type', $type);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Benefit Added"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get Single Benefit Data
+
+$app->get('/api/benefit/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "SELECT * from benefits WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $benefit = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($benefit);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Update Benefit
+
+$app->put('/api/benefit/update/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $title = $request->getParam('title');
+    $icon = $request->getParam('icon');
+    $type = $request->getParam('type');
+    $date = date('Y-m-d H:i:s');
+    $sql = "UPDATE benefits SET title=:title,icon=:icon,type=:type,modified='$date' WHERE id=$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':icon', $icon);
+        $stmt->bindParam(':type', $type);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Benefit Updated"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Delete Benefit
+
+$app->delete('/api/benefit/delete/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $sql = "DELETE from benefits WHERE id =$id";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $db = null;
+        echo '{"notice":{"text": "Benefit Deleted"}';
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+
+//Get All Benefit
+
+$app->get('/api/benefits', function (Request $request, Response $response) {
+    $sql = "SELECT * from benefits";
+    try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $benefits = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($benefits);
+    } catch (PDOException $e) {
+        echo '{"error":{"text": ' . $e->getMessage() . '}';
+    }
+});
+///////////////////////////////////////////////Benifit Exit///////////////////////////////////////////////
